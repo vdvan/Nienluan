d20848d117a5b21df09371b91a1375955c9447b0
vdvan <50206035+vdvan@users.noreply.github.com>
2019-05-06 00:14:11 +0700

    Create header.php

---
+++ server/view/pages/header.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo SITE_PATH ?>/view/vendor/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo SITE_PATH ?>/view/vendor/css/bootstrap-toggle.min.css">
+    <link rel="stylesheet" href="<?php echo SITE_PATH ?>/view/vendor/css/dashboard.css">
+    <link rel="stylesheet" href="<?php echo SITE_PATH ?>/view/vendor/css/style.css">
+
+    <title>Điều khiển thiết bị - Phan Ngọc Ái Vi</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
+        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Điều khiển thiết bị</a>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item mr-3">
+                <a class="nav-link" href="<?= SITE_PATH ?>/?mod=login&act=logout" onclick="return confirm('Bạn có muốn đăng xuất?')">Đăng xuất</a>
+            </li>
+        </ul>
+    </nav>
