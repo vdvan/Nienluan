cbe534936553b688222e369d6a7520f27672e006
vdvan <50206035+vdvan@users.noreply.github.com>
2019-05-06 00:14:07 +0700

    Create footer.php

---
+++ server/view/pages/footer.php
@@ -0,0 +1,20 @@
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="<?php echo SITE_PATH ?>/view/vendor/js/jquery-3.2.1.slim.min.js"></script>
+    <script src="<?php echo SITE_PATH ?>/view/vendor/js/popper.min.js"></script>
+    <script src="<?php echo SITE_PATH ?>/view/vendor/js/bootstrap.min.js"></script>
+    <script src="<?php echo SITE_PATH ?>/view/vendor/js/bootstrap4-toggle.min.js"></script>
+
+    <!-- Icons -->
+    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+    <script>
+      feather.replace()
+    </script>
+
+
+    <!-- Graphs -->
+    <script src="<?php echo SITE_PATH ?>/view/vendor/js/Chart.min.js"></script>
+    <script src="<?php echo SITE_PATH ?>/view/scripts/request_api.js"></script>
+    <?= $SITE_SCRIPTS; ?>
+  </body>
+</html>
