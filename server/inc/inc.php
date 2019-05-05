cc50a62882182fd4df4724708f16b70bd0f97899
vdvan <50206035+vdvan@users.noreply.github.com>
2019-05-06 00:12:58 +0700

    Create inc.php

---
+++ server/inc/inc.php
@@ -0,0 +1,9 @@
+<?php 
+if ( !defined('WEB') ) die("Access Denied");
+
+define('DB_HOST', 'localhost');
+define('DB_NAME','db_dieukhienthietbi');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
+
+define('SITE_PATH','http://localhost:8080/NLNganh/server');
